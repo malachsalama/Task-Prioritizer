e4c468123845c801f1537eb304777d46077daf8c
Malach <malach.salama@gmail.com>
2025-02-23 23:07:29 +0300

    core:update CORS configuration

--- main.py
+++ main.py
@@ -7,18 +7,28 @@ from core.config import settings
 
 app = FastAPI()
 
+# Define the allowed origins
+allowed_origins = [
+    "https://telex.im",
+    "https://staging.telex.im",
+    "https://telextest.im",
+    "https://staging.telextest.im"
+]
+
+# Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=allowed_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+# Include routers
 app.include_router(integration_router, prefix=settings.API_PREFIX)
 app.include_router(webhook_router, prefix=settings.API_PREFIX)
 
-
+# Health check endpoint
 @app.get("/healthcheck")
 async def health_check():
     """Checks if server is active."""
