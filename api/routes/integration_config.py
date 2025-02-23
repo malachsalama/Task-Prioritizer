b74d34ec66cbff3ec2d71e18a81c2f3032c0eeaf
Malach <malach.salama@gmail.com>
2025-02-23 09:02:21 +0300

    core:update integration json data

--- api/routes/integration_config.py
+++ api/routes/integration_config.py
@@ -15,48 +15,29 @@ integration_json = {
       "app_name": "Task Prioritizer",
       "app_description": "A custom integration that automatically notifies slack team members of High priority tasks from jira",
       "app_logo": "https://iili.io/dmHVsZG.png",
-      "app_url": "https://ec2-51-20-251-125.eu-north-1.compute.amazonaws.com",
+      "app_url": "http://ec2-51-20-251-125.eu-north-1.compute.amazonaws.com",
       "background_color": "#fff"
     },
     "is_active": True,
     "integration_type": "modifier",
     "integration_category": "Monitoring & Logging",
     "key_features": [
-      "automated alerts",
-      "priority tasks",
-      "real-time updates"
+      "auto alerts",
+      "realtime updates"
     ],
     "author": "Malach Salama",
     "settings": [
       {
-        "label": "Time Interval",
-        "type": "dropdown",
-        "required": True,
-        "default": "Immediate",
-        "options": [
-          "Immediate",
-          "Every 5-mins",
-          "Hourly"
-        ]
-      },
-      {
         "label": "slack channel",
         "type": "text",
         "required": True,
-        "default": "High priority tasks"
-      },
-      {
-        "label": "Logs",
-        "type": "checkbox",
-        "required": True,
-        "default": "true"
+        "default": "high priority task"
       }
     ],
     "target_url": settings.TELEX_WEBHOOK_URL,
     "tick_url": settings.TICK_URL
   }
 }
-
 @router.get("/integration-config")
 async def get_integration_json():
     return JSONResponse(content=integration_json)
